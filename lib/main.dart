6b28fe56de6aadc92417c962248fefba313ac5c6
Shimon <shimonbhuiya02@gmail.com>
2025-07-30 19:53:08 +0600

    Snackbar and alertDialog Complete

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Popupdemo.dart';
+
+
+void main(){
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget{
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Popupdemo());
+  }
+
+}
+
