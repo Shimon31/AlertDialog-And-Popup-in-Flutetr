39949da4b990ca96e775005173c6fd4b5216e969
Shimon <shimonbhuiya02@gmail.com>
2025-07-31 17:26:50 +0600

    All Complete

--- lib/Popupdemo.dart
+++ lib/Popupdemo.dart
@@ -2,84 +2,79 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Popupdemo extends StatelessWidget {
-
-
   //Alert dialog
   void ShowAlertDialog(BuildContext context) {
-    showDialog(context: context,
-        builder: (BuildContext context) =>
-            AlertDialog(
-              title: Text("Alert"),
-              content: Text("Do you want to exit this Dialog?"),
-              actions: [
-
-                TextButton(onPressed: () => Navigator.pop(context),
-                    child: Text("Yes")),
-                TextButton(onPressed: () => Navigator.pop(context),
-                    child: Text("No"))
-
-              ],
-            ));
-  }
-
-
-  void ShowSimpleDialog(BuildContext context) {
-
-   showDialog(
-       context: context, 
-       builder: (BuildContext context){
-         return SimpleDialog(
-           title: Text("Show Simple Dialog"),
-           children: [
-             SimpleDialogOption(
-               onPressed: ()=>Navigator.pop(context,"Option A"),
-               child: Text("Option A"),
-             ),
-             
-             SimpleDialogOption(
-               onPressed: ()=>Navigator.pop(context,"Option B"),
-               child: Text("Option B"),
-             ),
-             SimpleDialogOption(
-               onPressed: ()=>Navigator.pop(context,"Option C"),
-               child: Text("Option C"),
-             ),
-           ],
-           
-         );
-       });
-        
-
-  }
-
-
-//Snack bar
-  void ShowSnackBar(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-
-        SnackBar(content: Text("Clicked Successfully"),
-            duration: Duration(seconds: 1) ,)
+    showDialog(
+      context: context,
+      builder:
+          (BuildContext context) => AlertDialog(
+            title: Text("Alert"),
+            content: Text("Do you want to exit this Dialog?"),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Yes"),
+                style: TextButton.styleFrom(foregroundColor: Colors.red),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("No"),
+              ),
+            ],
+          ),
     );
   }
 
+  void ShowSimpleDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text("Show Simple Dialog"),
+          children: [
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, "Option A"),
+              child: Text("Option A"),
+            ),
 
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, "Option B"),
+              child: Text("Option B"),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, "Option C"),
+              child: Text("Option C"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //Snack bar
+  void ShowSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Clicked Successfully"),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
 
   void ShowBottomSheet(BuildContext context) {
-
     showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context){
-          return Container(
-           padding: EdgeInsets.all(10),
-           height: 500,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-               Container(height: 50,color: Colors.red,)
-              ],
-            ),
-          );
-        });
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(10),
+          height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Container(height: 50, color: Colors.red)],
+          ),
+        );
+      },
+    );
   }
 
   const Popupdemo({super.key});
@@ -102,7 +97,14 @@ class Popupdemo extends StatelessWidget {
 
             ElevatedButton(
               onPressed: () => ShowSimpleDialog(context),
-              child: Text("Show Simple Dialog"),
+              child: Text(
+                "Show Simple Dialog",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                elevation: 10,
+              ),
             ),
             SizedBox(height: 20),
 
